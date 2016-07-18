5cb09164e2ad86435a1783b83268a9b29d8a7c67
Cauterite <Cauterite@gmail.com>
2016-07-18 22:49:56 +1000

    Update lua.h

--- src/lua.h
+++ src/lua.h
@@ -266,8 +266,8 @@ LUA_API int   (lua_setmetatable) (lua_State *L, int objindex);
 LUA_API void  (lua_setuservalue) (lua_State *L, int idx);
 
 
-LUA_API int   (lua_isfrozentable) (lua_State *L, int idx);
-LUA_API int   (lua_freezetable) (lua_State *L, int idx);
+LUA_API void  (lua_isfrozentable) (lua_State *L, int idx);
+LUA_API void  (lua_freezetable) (lua_State *L, int idx);
 
 
 /*
